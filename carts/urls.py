e4081b9d32913d92781ad1a35c1441fbc66e9aae
fox <artemdav2@gmail.com>
2022-10-02 15:27:10 +0600

    carts

--- carts/urls.py
+++ carts/urls.py
@@ -6,6 +6,7 @@ from carts import views
 
 urlpatterns = [
     path('', views.cart, name='cart'),
+    path('add_cart/<int:product_id>/', views.add_cart, name='add_cart'),
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
