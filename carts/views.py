68e0d6e100fa5504e31461be53d0513c5936d96d
fox <artemdav2@gmail.com>
2022-10-02 20:04:14 +0600

    carts

--- carts/views.py
+++ carts/views.py
@@ -4,8 +4,22 @@ from pages.models import Product
 from carts.models import Cart, CartItem
 # Create your views here.
 
-def cart(request):
-    return render(request, 'store/cart.html')
+def cart(request, total=0, quantity=0, cart_items=None):
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
+    except ObjectNotExist:
+        pass
+
+    context ={
+        'total':total,
+        'quantity': quantity,
+        'cart_items': cart_items,
+    }
+    return render(request, 'store/cart.html', context)
 
 def _cart_id(request):
     cart = request.session.session_key
@@ -35,6 +49,6 @@ def add_cart(reguest, product_id):
             cart = cart,
         )
         cart_item.save()
-    return HttpResponse(cart_item.quantity)
-    exit()
+    # return HttpResponse(cart_item.quantity)
+    # exit()
     return redirect('cart')
