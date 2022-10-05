9d341a5477425f11c29c5c7492697f1bce91e8bb
fox <artemdav2@gmail.com>
2022-10-05 16:47:42 +0600

    comit

--- pages/admin.py
+++ pages/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from pages .models import Category, Product
+from pages .models import Category, Product, Variation
 
 # Register your models here.
 
@@ -20,3 +20,10 @@ class ProductAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Product, ProductAdmin)
+
+class VariationAdmin(admin.ModelAdmin):
+    list_display = ('product', 'variation_category', 'variation_value', 'is_active', )
+    list_editable = ('is_active',)
+    list_filter = ('product', 'variation_category', 'variation_value',)
+
+admin.site.register(Variation, VariationAdmin)
